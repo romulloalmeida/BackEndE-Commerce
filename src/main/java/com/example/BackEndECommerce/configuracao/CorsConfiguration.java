6806c5709377c58b87bf8716877860ca9920e0f3
romulloalmeida <romulloalmeida@bol.com>
2021-12-10 09:53:23 -0300

    CORS

---
+++ src/main/java/com/example/BackEndECommerce/configuracao/CorsConfiguration.java
@@ -0,0 +1,16 @@
+package com.example.BackEndECommerce.configuracao;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //liberando app cliente 1
+        registry.addMapping("/")
+                .allowedOrigins("https://ctdcommerce.vercel.app/*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+}
